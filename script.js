fe81909f11c9cdebfb79e2d8152d3374831ed3c1
M. Witte Paz <mathiaswittepaz@gmail.com>
2022-07-26 13:32:53 +0200

    Modifying Simons Example

--- script.js
+++ script.js
@@ -1,137 +1,153 @@
-import { DATA } from './data.js';
+import { DATA } from "./data.js";
 
 var CHART;
 var CHART_OPTION = {
   grid: [
-    {
-      id: "ptmMap",
-      width: "800px",
-      height: "800px",
-      bottom: "50px",
-      left: "50px",
-      show: true,
-      backgroundColor: "#EFEFEF"
-    },
+    // {
+    //   id: "ptmMap",
+    //   width: "700px",
+    //   height: "700px",
+    //   bottom: "50px",
+    //   left: "50px",
+    //   top: "50px",
+    //   // left: "50px",
+    //   // show: true,
+    //   backgroundColor: "#EFEFEF",
+    // },
     {
       id: "ptmStackedBar",
-      width: "800px",
+      width: "1000px",
       height: "100px",
       bottom: "870px",
-      left: '50px'
-    }
+      // show: true,
+      top: "50px",
+
+      left: "50px",
+    },
   ],
   xAxis: [
-    {
-      id: "ptmMapX",
-      type: 'category',
-      data: [],
-      gridIndex: 0
-    },
+    // {
+    //   id: "ptmMapX",
+    //   type: "category",
+    //   data: [],
+    //   gridIndex: 0,
+    // },
     {
       id: "ptmStackedBarX",
-      type: 'category',
+      type: "category",
       data: [],
       show: false,
-      gridIndex: 1
-    }
+      gridIndex: 0,
+    },
   ],
   yAxis: [
-    {
-      id: "ptmMapY",
-      type: 'category',
-      data: [],
-      inverse: true,
-      gridIndex: 0
-    },
+    // {
+    //   id: "ptmMapY",
+    //   type: "category",
+    //   data: [],
+    //   inverse: true,
+    //   gridIndex: 0,
+    // },
     {
       id: "ptmStackedBarY",
-      type: 'value',
-      gridIndex: 1
-    }
+      type: "value",
+      gridIndex: 0,
+    },
   ],
   toolbox: {
     feature: {
       dataZoom: {
         xAxisIndex: 0,
-        yAxisIndex: 0
-      }
-    }
+        yAxisIndex: 0,
+      },
+    },
   },
   dataZoom: [
     {
-      type: 'inside',
+      type: "inside",
       show: true,
-      xAxisIndex: 1
-    }
+      xAxisIndex: 1,
+    },
   ],
-  visualMap: {
-    min: 0,
-    max: 1,
-    dimension: 2,
-    seriesIndex: 0,
-    hoverLink: true,
-    inverse: false,
-    orient: 'vertical',
-    itemHeight: 800,
-    top: 'center',
-    right: 'right',
-    text: ['2', '1'],
-    textStyle: {
-      color: '#607196',
-      fontSize: 12
-    },
-    calculable: true,
-    inRange: {
-      color: ['#E8DAB2', '#BF545B', '#BF5483', '#9454BF', '#5474BF']
-    },
-    formatter: (value) => {
-      return Math.round(value);
-    }
-  },
+  // visualMap: {
+  //   min: 0,
+  //   max: 1,
+  //   dimension: 2,
+  //   seriesIndex: 0,
+  //   hoverLink: true,
+  //   inverse: false,
+  //   orient: "vertical",
+  //   itemHeight: 800,
+  //   top: "center",
+  //   right: "right",
+  //   text: ["2", "1"],
+  //   textStyle: {
+  //     color: "#607196",
+  //     fontSize: 12,
+  //   },
+  //   calculable: true,
+  //   inRange: {
+  //     color: ["#E8DAB2", "#BF545B", "#BF5483", "#9454BF", "#5474BF"],
+  //   },
+  //   formatter: (value) => {
+  //     return Math.round(value);
+  //   },
+  // },
   series: [
-    {
-      name: 'PTMContactMap',
-      type: 'heatmap',
-      data: []
-    }
-  ]
+    // {
+    //   name: "PTMContactMap",
+    //   type: "heatmap",
+    //   data: [],
+    // },
+  ],
 };
 var PTMStackedBars = {};
-var PTMLine = { };
+var PTMLine = {};
+var PTMAreaChart = {};
+var OverallPTMs = {};
 
-window.onload = _ => {
-  CHART = echarts.init(document.getElementById("chartContainer"), { "renderer": "canvas" });
+window.onload = (_) => {
+  CHART = echarts.init(document.getElementById("chartContainer"), { renderer: "canvas" });
   document.getElementById("accSelect").onchange = (event) => {
-    if (event.isTrusted && event.type == 'change') {
+    if (event.isTrusted && event.type == "change") {
       updateChart(document.getElementById("accSelect").value);
     }
   };
-  CHART.on('datazoom', (event) => {
+  CHART.on("datazoom", (event) => {
     if (event.dataZoomIndex == 0) {
       return;
     }
     let zoomFactor = -1;
-    if (event.batch[0].dataZoomId == '\x00_ec_\x00toolbox-dataZoom_xAxis0' && event.batch[1].dataZoomId == '\x00_ec_\x00toolbox-dataZoom_yAxis0') {
-      if (event.batch[0].startValue && event.batch[0].endValue && event.batch[1].startValue && event.batch[1].endValue) {
+    if (
+      event.batch[0].dataZoomId == "\x00_ec_\x00toolbox-dataZoom_xAxis0" &&
+      event.batch[1].dataZoomId == "\x00_ec_\x00toolbox-dataZoom_yAxis0"
+    ) {
+      if (
+        event.batch[0].startValue &&
+        event.batch[0].endValue //&&
+        // event.batch[1].startValue &&
+        // event.batch[1].endValue
+      ) {
         CHART.dispatchAction({
-          type: 'dataZoom',
+          type: "dataZoom",
           dataZoomIndex: 0,
           startValue: event.batch[0].startValue,
-          endValue: event.batch[0].endValue
+          endValue: event.batch[0].endValue,
         });
         zoomFactor = event.batch[0].endValue - event.batch[0].startValue;
       } else {
         CHART.dispatchAction({
-          type: 'dataZoom',
+          type: "dataZoom",
           dataZoomIndex: 0,
           start: event.batch[0].start,
-          end: event.batch[0].end
+          end: event.batch[0].end,
         });
-        zoomFactor = ((event.batch[0].end - event.batch[0].start) / 100) * CHART_OPTION.xAxis[0].data.length;
+        zoomFactor =
+          ((event.batch[0].end - event.batch[0].start) / 100) * CHART_OPTION.xAxis[0].data.length;
       }
     }
-    if ( zoomFactor <= 50 ) {
-      addStackedBar( );
+    if (zoomFactor <= 50) {
+      addStackedBar();
     } else {
       cleanStackedBar();
     }
@@ -141,86 +157,128 @@ window.onload = _ => {
 function updateChart(proteinAcc) {
   CHART_OPTION.xAxis[0].data = [];
   CHART_OPTION.yAxis[0].data = [];
-  CHART_OPTION.series[0].data = [];
-  CHART_OPTION.xAxis[1].data = [];
-  CHART_OPTION.yAxis[1].data = [];
-  CHART_OPTION.visualMap.max = 0;
+  // CHART_OPTION.series[0].data = [];
+  // CHART_OPTION.xAxis[1].data = [];
+  // CHART_OPTION.yAxis[1].data = [];
+  // CHART_OPTION.visualMap.max = 0;
   PTMStackedBars = {};
-  PTMLine = {
-    name: 'PTMLine',
-    type: 'line',
-    xAxisIndex: 1,
-    yAxisIndex: 1,
-    data: [ ],
-    showSymbol: false,
-    smooth: true,
-    lineStyle: {
-      color: '#444444'
-    }
-  };
+  PTMAreaChart = {};
+  // PTMLine = {
+  //   name: "PTMLine",
+  //   type: "line",
+  //   xAxisIndex: 1,
+  //   yAxisIndex: 1,
+  //   data: [],
+  //   showSymbol: false,
+  //   smooth: true,
+  //   lineStyle: {
+  //     color: "#444444",
+  //   },
+  // };
   for (let residue in DATA[proteinAcc].residues) {
-    DATA[proteinAcc].residues[residue].ptm.forEach(ptm => {
-      PTMStackedBars[ptm] = []
+    DATA[proteinAcc].residues[residue].ptm.forEach((ptm) => {
+      PTMStackedBars[ptm] = [];
+      PTMAreaChart[ptm] = [];
     });
-    PTMLine.data.push( DATA[proteinAcc].residues[residue].ptm.length );
+    // PTMLine.data.concat(DATA[proteinAcc].residues[residue].ptm);
   }
+
   for (let residue in DATA[proteinAcc].residues) {
+    // CHART_OPTION.xAxis[0].data.push(residue.split("@")[1] + " (" + residue.split("@")[0] + ")");
+    // CHART_OPTION.yAxis[0].data.push(residue.split("@")[1] + " (" + residue.split("@")[0] + ")");
     CHART_OPTION.xAxis[0].data.push(residue.split("@")[1] + " (" + residue.split("@")[0] + ")");
-    CHART_OPTION.yAxis[0].data.push(residue.split("@")[1] + " (" + residue.split("@")[0] + ")");
-    CHART_OPTION.xAxis[1].data.push(residue.split("@")[1] + " (" + residue.split("@")[0] + ")");
     let ptmCounts = {};
-    DATA[proteinAcc].residues[residue].ptm.forEach(ptm => {
+    DATA[proteinAcc].residues[residue].ptm.forEach((ptm) => {
       if (ptmCounts[ptm]) {
         ptmCounts[ptm]++;
       } else {
         ptmCounts[ptm] = 1;
       }
+      if (OverallPTMs[ptm]) {
+        OverallPTMs[ptm]++;
+      } else {
+        OverallPTMs[ptm] = 1;
+      }
     });
     for (let ptm of Object.keys(PTMStackedBars)) {
       if (ptm in ptmCounts) {
         PTMStackedBars[ptm].push(ptmCounts[ptm]);
+        PTMAreaChart[ptm].push(ptmCounts[ptm]);
       } else {
-        PTMStackedBars[ptm].push(0)
+        PTMStackedBars[ptm].push(0);
+        PTMAreaChart[ptm].push(0);
       }
     }
-    for (let contactResidue of DATA[proteinAcc].residues[residue].contacts) {
-      let residueNumber = parseInt(residue.split("@")[1]);
-      let contactResidueNumber = parseInt(contactResidue.split("@")[1]);
-      let residuePTM = DATA[proteinAcc].residues[residue].ptm;
-      let contactResiduePTM = DATA[proteinAcc].residues[contactResidue].ptm;
-      if (residuePTM.length > 0 && contactResiduePTM.length > 0) {
-        let PTMSimilarity = computePTMIntersection(residuePTM, contactResiduePTM);
-        CHART_OPTION.series[0].data.push([
-          residueNumber,
-          contactResidueNumber,
-          PTMSimilarity
-        ])
-      }
-    }
+    // for (let contactResidue of DATA[proteinAcc].residues[residue].contacts) {
+    //   let residueNumber = parseInt(residue.split("@")[1]);
+    //   let contactResidueNumber = parseInt(contactResidue.split("@")[1]);
+    //   let residuePTM = DATA[proteinAcc].residues[residue].ptm;
+    //   let contactResiduePTM = DATA[proteinAcc].residues[contactResidue].ptm;
+    //   if (residuePTM.length > 0 && contactResiduePTM.length > 0) {
+    //     let PTMSimilarity = computePTMIntersection(residuePTM, contactResiduePTM);
+    //     CHART_OPTION.series[0].data.push([residueNumber, contactResidueNumber, PTMSimilarity]);
+    //   }
+    // }
   }
+
+  // addAreaChart();
+  // console.log(PTMAreaChart);
+
   cleanStackedBar();
 }
 
 function cleanStackedBar() {
-  CHART_OPTION.series = CHART_OPTION.series.filter(s => !s.name.startsWith("PTMStackedBar"));
-  CHART_OPTION.series.push( PTMLine );
-  CHART.setOption(CHART_OPTION, { replaceMerge: [ 'series' ] } );
+  CHART_OPTION.series = CHART_OPTION.series.filter((s) => !s.name.startsWith("PTMStackedBar"));
+  // CHART_OPTION.series.push(PTMAreaChart);
+  addAreaChart();
+}
+
+function cleanAreaChart() {
+  CHART_OPTION.series = CHART_OPTION.series.filter((s) => !s.name.startsWith("PTMAreaChart"));
+  // CHART_OPTION.series.push(PTMAreaChart);
+  // CHART.setOption(CHART_OPTION, { replaceMerge: ["series"] });
 }
 
 function addStackedBar() {
-  CHART_OPTION.series.splice( 1, 1 );
+  cleanAreaChart();
   for (let ptm of Object.keys(PTMStackedBars)) {
     CHART_OPTION.series.push({
       name: "PTMStackedBar_" + ptm,
-      type: 'bar',
-      stack: 'total',
+      type: "bar",
+      stack: "Total",
       data: PTMStackedBars[ptm],
-      xAxisIndex: 1,
-      yAxisIndex: 1,
-      large: true
+      xAxisIndex: 0,
+      yAxisIndex: 0,
+      large: true,
     });
   }
-  CHART.setOption(CHART_OPTION, { replaceMerge: [ 'series' ] });
+  CHART.setOption(CHART_OPTION, { replaceMerge: ["series"] });
+}
+
+function addAreaChart() {
+  for (let ptm of Object.keys(PTMAreaChart)) {
+    CHART_OPTION.series.push({
+      name: "PTMAreaChart_" + ptm,
+      type: "line",
+      stack: "Total",
+      smooth: true,
+      areaStyle: {},
+      data: PTMAreaChart[ptm],
+      // xAxisIndex: 1,
+      // yAxisIndex: 1,
+      large: true,
+    });
+  }
+  var overallOrder = Object.keys(OverallPTMs).map((x) => [x, OverallPTMs[x]]);
+  overallOrder.sort((x, y) => x[1] - y[1]);
+  CHART_OPTION.series.sort(
+    (x, y) => getPTMorder(x["name"], overallOrder) - getPTMorder(y["name"], overallOrder)
+  );
+  CHART.setOption(CHART_OPTION, { replaceMerge: ["series"] });
+}
+
+function getPTMorder(name, overallOrder) {
+  return overallOrder.indexOf(name.split("_")[1]);
 }
 
 function computePTMUnion(riPTM, rjPTM) {
@@ -234,7 +292,7 @@ function computePTMUnion(riPTM, rjPTM) {
 }
 
 function computePTMIntersection(riPTM, rjPTM) {
-  let intersection = riPTM.filter(v => rjPTM.includes(v));
+  let intersection = riPTM.filter((v) => rjPTM.includes(v));
   let intersectionSize = intersection.length;
   if (intersectionSize > CHART_OPTION.visualMap.max) {
     CHART_OPTION.visualMap.max = intersectionSize;
