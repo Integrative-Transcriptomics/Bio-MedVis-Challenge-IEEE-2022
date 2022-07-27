3c581764463916e4b5b9ef89e53ed4ddc676f637
s-t-h <si.ha@live.de>
2022-07-27 11:14:31 +0200

    [UPDATE] Added mode to switch between intersection/union to display PTM similarity, added labels, added (dummy) tooltip

--- script.js
+++ script.js
@@ -2,22 +2,34 @@ import { DATA } from './data.js';
 
 var CHART;
 var CHART_OPTION = {
+  title: [
+    {
+      show: true,
+      bottom: '20px',
+      left: '980px',
+      text: 'Set Intersection Size of PTMs',
+      textStyle: {
+        fontSize: 14
+      },
+      textAlign: 'left'
+    }
+  ],
   grid: [
     {
       id: "ptmMap",
-      width: "800px",
-      height: "800px",
-      bottom: "50px",
-      left: "50px",
+      width: "750px",
+      height: "750px",
+      bottom: "80px",
+      left: "100px",
       show: true,
       backgroundColor: "#EFEFEF"
     },
     {
       id: "ptmStackedBar",
-      width: "800px",
+      width: "750px",
       height: "100px",
       bottom: "870px",
-      left: '50px'
+      left: '100px'
     }
   ],
   xAxis: [
@@ -25,7 +37,14 @@ var CHART_OPTION = {
       id: "ptmMapX",
       type: 'category',
       data: [],
-      gridIndex: 0
+      gridIndex: 0,
+      name: 'Residue Index (Residue Type)',
+      nameLocation: 'middle',
+      nameGap: 40,
+      nameTextStyle: {
+        fontWeight: 'bold',
+        fontSize: 14
+      }
     },
     {
       id: "ptmStackedBarX",
@@ -41,16 +60,72 @@ var CHART_OPTION = {
       type: 'category',
       data: [],
       inverse: true,
-      gridIndex: 0
+      gridIndex: 0,
+      name: 'Residue Index (Residue Type)',
+      nameLocation: 'middle',
+      nameGap: 60,
+      nameTextStyle: {
+        fontWeight: 'bold',
+        fontSize: 14
+      }
     },
     {
       id: "ptmStackedBarY",
       type: 'value',
-      gridIndex: 1
+      gridIndex: 1,
+      name: 'No. PTMs',
+      nameLocation: 'middle',
+      nameGap: 60,
+      nameTextStyle: {
+        fontWeight: 'bold',
+        fontSize: 14
+      },
+      minInterval: 1
     }
   ],
+  axisPointer: {
+    show: true,
+    triggerOn: 'click',
+    z: 5,
+    triggerTooltip: true,
+    link: [
+      {
+        xAxisIndex: [ 0, 1 ]
+      }
+    ]
+  },
+  tooltip: {
+    show: true,
+    position: [ 900, 50 ],
+    extraCssText: 'height: 800px; width: 950px'
+  },
   toolbox: {
     feature: {
+      myTool1: {
+        show: true,
+        title: 'Display Mode: Intersection',
+        icon: 'path://M432.45,595.444c0,2.177-4.661,6.82-11.305,6.82c-6.475,0-11.306-4.567-11.306-6.82s4.852-6.812,11.306-6.812C427.841,588.632,432.452,593.191,432.45,595.444L432.45,595.444z M421.155,589.876c-3.009,0-5.448,2.495-5.448,5.572s2.439,5.572,5.448,5.572c3.01,0,5.449-2.495,5.449-5.572C426.604,592.371,424.165,589.876,421.155,589.876L421.155,589.876z M421.146,591.891c-1.916,0-3.47,1.589-3.47,3.549c0,1.959,1.554,3.548,3.47,3.548s3.469-1.589,3.469-3.548C424.614,593.479,423.062,591.891,421.146,591.891L421.146,591.891zM421.146,591.891',
+        onclick: function( ) {
+          if ( PTMSimilarityState == 'intersection' ) {
+            PTMSimilarityState = 'union';
+            computePTMSimilarity = (riPTM, rjPTM) => {
+              return computePTMUnion( riPTM, rjPTM );  
+            };
+            CHART_OPTION.toolbox.feature.myTool1.title = 'Mode: Union';
+            CHART_OPTION.title[ 0 ].text = 'Set Union Size of PTMs';
+            updateChart(document.getElementById("accSelect").value);
+          } else if ( PTMSimilarityState == 'union' ) {
+            PTMSimilarityState = 'intersection';
+            computePTMSimilarity = (riPTM, rjPTM) => {
+              return computePTMIntersection( riPTM, rjPTM );  
+            };
+            CHART_OPTION.toolbox.feature.myTool1.title = 'Mode: Intersection';
+            CHART_OPTION.title[ 0 ].text = 'Set Intersection Size of PTMs';
+            updateChart(document.getElementById("accSelect").value);
+          }
+
+        }
+      },
       dataZoom: {
         xAxisIndex: 0,
         yAxisIndex: 0
@@ -71,10 +146,10 @@ var CHART_OPTION = {
     seriesIndex: 0,
     hoverLink: true,
     inverse: false,
-    orient: 'vertical',
+    orient: 'horizontal',
     itemHeight: 800,
-    top: 'center',
-    right: 'right',
+    bottom: '60px',
+    right: '80px',
     text: ['2', '1'],
     textStyle: {
       color: '#607196',
@@ -92,12 +167,17 @@ var CHART_OPTION = {
     {
       name: 'PTMContactMap',
       type: 'heatmap',
-      data: []
+      data: [],
+      itemStyle: {
+        borderColor: '#CCCCCC',
+        borderWidth: 0.3
+      }
     }
   ]
 };
 var PTMStackedBars = {};
 var PTMLine = { };
+var PTMSimilarityState = 'intersection';
 
 window.onload = _ => {
   CHART = echarts.init(document.getElementById("chartContainer"), { "renderer": "canvas" });
@@ -130,7 +210,7 @@ window.onload = _ => {
         zoomFactor = ((event.batch[0].end - event.batch[0].start) / 100) * CHART_OPTION.xAxis[0].data.length;
       }
     }
-    if ( zoomFactor <= 50 ) {
+    if ( zoomFactor <= 30 ) {
       addStackedBar( );
     } else {
       cleanStackedBar();
@@ -139,6 +219,7 @@ window.onload = _ => {
 };
 
 function updateChart(proteinAcc) {
+  CHART_OPTION.series = [ CHART_OPTION.series[ 0 ] ];
   CHART_OPTION.xAxis[0].data = [];
   CHART_OPTION.yAxis[0].data = [];
   CHART_OPTION.series[0].data = [];
@@ -153,9 +234,15 @@ function updateChart(proteinAcc) {
     yAxisIndex: 1,
     data: [ ],
     showSymbol: false,
-    smooth: true,
+    smooth: 1,
+    smoothMonotone: 'x',
     lineStyle: {
-      color: '#444444'
+      color: '#666666',
+      width: 1
+    },
+    areaStyle: {
+      color: '#999999',
+      opacity: 0.5
     }
   };
   for (let residue in DATA[proteinAcc].residues) {
@@ -189,7 +276,7 @@ function updateChart(proteinAcc) {
       let residuePTM = DATA[proteinAcc].residues[residue].ptm;
       let contactResiduePTM = DATA[proteinAcc].residues[contactResidue].ptm;
       if (residuePTM.length > 0 && contactResiduePTM.length > 0) {
-        let PTMSimilarity = computePTMIntersection(residuePTM, contactResiduePTM);
+        let PTMSimilarity = computePTMSimilarity(residuePTM, contactResiduePTM);
         CHART_OPTION.series[0].data.push([
           residueNumber,
           contactResidueNumber,
@@ -222,6 +309,10 @@ function addStackedBar() {
   }
   CHART.setOption(CHART_OPTION, { replaceMerge: [ 'series' ] });
 }
+
+var computePTMSimilarity = (riPTM, rjPTM) => {
+  return computePTMIntersection( riPTM, rjPTM );  
+};
 
 function computePTMUnion(riPTM, rjPTM) {
   let union = [...new Set([...riPTM, ...rjPTM])];
